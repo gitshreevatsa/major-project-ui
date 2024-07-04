a9f24e0688912359ad705d98a0503d62f8c2881b
Shree Vatsa <vastas2002@gmail.com>
2024-07-04 12:35:54 +0530

    ui

--- src/Filemanager.js
+++ src/Filemanager.js
@@ -1,5 +1,3 @@
-// FileManagerPage.js
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./Filemanager.css"; // Import the CSS file
@@ -7,12 +5,12 @@ import lighthouse from "@lighthouse-web3/sdk";
 
 const FileManagerPage = () => {
   const [files, setFiles] = useState([]);
-
   const [fileName, setFileName] = useState("");
   const [cid, setCid] = useState("");
+  const [loading, setLoading] = useState(true);
 
   const walletAddress = localStorage.getItem("WalletAddress");
-  console.log(walletAddress);
+
   useEffect(() => {
     const fetchFiles = async () => {
       try {
@@ -22,13 +20,17 @@ const FileManagerPage = () => {
         const filesData = response.data.fileNameToCID; // Assuming response.data contains the filename:CID pairs
         console.log(filesData);
         // Update the file URLs with IPFS gateway URLs + CIDs
-        const updatedFiles = Object.entries(filesData).map(([fileName, cid]) => ({
+        const updatedFiles = Object.entries(filesData).map(
+          ([fileName, cid]) => ({
             name: fileName,
             url: `https://gateway.lighthouse.storage/ipfs/${cid}`,
-          }));
-          setFiles(updatedFiles);
+          })
+        );
+        setFiles(updatedFiles);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching files:", error);
+        setLoading(false);
       }
     };
 
@@ -47,6 +49,7 @@ const FileManagerPage = () => {
     e.preventDefault();
     if (cid.length === 0) {
       alert("Please select a file or wait till it is uploaded");
+      return;
     }
     console.log(cid, fileName);
     try {
@@ -55,14 +58,11 @@ const FileManagerPage = () => {
         contentID: cid,
         filename: fileName,
       };
-      axios
-        .post("http://localhost:5001/api/upload", postData) // change api url
-        .then((response) => {
-          console.log("Post request successful:", response.data);
-        })
-        .catch((error) => {
-          console.error("Error posting data:", error);
-        });
+      const response = await axios.post(
+        "http://localhost:5001/api/upload",
+        postData
+      );
+      console.log("Post request successful:", response.data);
     } catch (error) {
       console.error("Error uploading file:", error);
       alert("Error uploading file. Please try again.");
@@ -70,6 +70,13 @@ const FileManagerPage = () => {
   };
 
   const specialUpload = async (fi) => {
+    const fileNameElement = document.getElementById("fileName");
+    if (fi.length > 0) {
+      const fileName = fi[0].name;
+      fileNameElement.textContent = "Selected file: " + fileName;
+    } else {
+      fileNameElement.textContent = ""; // Clear the text if no file is selected
+    }
     const output = await lighthouse.upload(
       fi,
       "4ebd3b81.03a927d43e4a492ebe8b0500947957f0",
@@ -79,10 +86,11 @@ const FileManagerPage = () => {
     );
     console.log("File Status:", output);
     console.log("filename", output.data.Name);
+
     setCid(output.data.Hash);
     setFileName(output.data.Name);
-    if(output.data.Hash.length > 0){
-
+    if (output.data.Hash.length > 0) {
+      // Do something after file upload
     }
   };
 
@@ -90,39 +98,59 @@ const FileManagerPage = () => {
     <div className="container">
       <h2>Your Files</h2>
       <h4>Store your files encrypted on the blockchain</h4>
-      {files.length > 0 ? (
+      {loading ? (
+        <p>Loading...</p>
+      ) : files.length > 0 ? (
         <div className="file-list">
-          {files.map((file, index) => (
-            <div className="file-item" key={index}>
-              <a
-                className="file-link"
-                href={file.url}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {file.name}
-              </a>
-            </div>
-          ))}
+          <table>
+            <thead>
+              <tr>
+                <th>File Name</th>
+                <th>Download Link</th>
+              </tr>
+            </thead>
+            <tbody>
+              {files.map((file, index) => (
+                <tr key={index}>
+                  <td>{file.name}</td>
+                  <td>
+                    <a
+                      href={file.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Download
+                    </a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       ) : (
-        <p className="no-files-message">
-          No files are saved with this address.
-        </p>
+        <div className="no-files-message">
+          <p>No files are saved with this address.</p>
+        </div>
       )}
       <form className="upload-form" onSubmit={handleUpload}>
         <input
           className="upload-input"
           type="file"
           name="file"
+          id="fileInput"
           onChange={(e) => {
             specialUpload(e.target.files);
           }}
         />
-        <button className="upload-btn" type="submit" onClick={handleUpload}>
+        <label htmlFor="fileInput" className="upload-label">
+          Choose File
+        </label>
+        <button className="upload-btn" type="submit">
           Upload File
         </button>
       </form>
+      <p id="fileName"></p>
+
     </div>
   );
 };
